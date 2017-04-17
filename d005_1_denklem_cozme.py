b96688b93e2eb3ace1e1736d31e78b7f48cb9835
omergaffar <omergaffar0064@gmail.com>
2017-04-17 18:38:22 +0300

    Update d005_1_denklem_cozme.py

--- d005_1_denklem_cozme.py
+++ d005_1_denklem_cozme.py
@@ -5,16 +5,26 @@ Created on Sun Mar 19 16:15:23 2017
 
 @author: acs
 """
-
+"""
+Burada denklem çözmede daireyi yapacağız.
+İlk olarak matematik olarak tanımlıyoruz.
+Numpy kütüphanesini ekliyoruz.
+Koordinat düzleminde x ve y'yi tanımlıyoruz.
+Arange kullanarak dairenin matematiksel işlemlerini yazıyoruz.
+En son olarakda for döngüsüyle koordinat sisteminde yerini belirliyoruz.
+"""
 from sympy.solvers import solve
+# sympy solvers komutuyla çalıştırmaya yarar.
 from sympy import Symbol
 import numpy as np
 from matplotlib import pyplot as plt
-
 x = Symbol('x')
+# x'i symbolle tanımlattı
 y = np.arange(-1.0,1.01,0.1)
+# arange komutuyla daire nin matematiksel komutları girildi.
 xp=[]
 xn=[]
+#for döngüsüyle koordinat düzleminde dairenin yeri belirlendi.
 for yi in y:
     sol=solve(x**2 +yi**2-1.0,x)
     print sol
@@ -23,5 +33,7 @@ for yi in y:
     print xp[-1],xn[-1]
 plt.plot(xp,y)
 plt.plot(xn,y)
+
+
 
 
